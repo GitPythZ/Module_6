b99cf413344f662ebbabf575e39c0533a72a0c09
Anatoliy_Yavorskiy <yardbirds1957@gmail.com>
2024-09-12 15:09:04 +0300

    Домашняя работа

--- module_6_1.py
+++ module_6_1.py
@@ -1,6 +1,6 @@
 #Задача "Съедобное, несъедобное"
 class Animal:
-    def __init__(self, name, alive = True, fed = False):
+    def __init__(self, name, alive=True, fed=False):
         self.name = name
         self.fed = fed
         self.alive = alive
@@ -8,6 +8,7 @@ class Animal:
     def eat(self, food):
         if food.edible:
             print(f"{self.name} съел {food.name}")
+            self.fed = True
         else:
             print(f"{self.name} не стал есть {food.name}")
             self.alive = False
