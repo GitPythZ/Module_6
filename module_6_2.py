bde7c943af9ff45571f6c1efc08669580c359adc
Anatoliy_Yavorskiy <yardbirds1957@gmail.com>
2024-09-12 16:05:32 +0300

    Домашняя работа

---
+++ module_6_2.py
@@ -0,0 +1,43 @@
+#Задача "Изменять нельзя получать"
+class Vehicle():
+    __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
+    def __init__(self,  owner, __model, __engine_power, __colour):
+        self.owner = str(owner)
+        self.__model = str(__model)
+        self.__engine_power = int(__engine_power)
+        self.__colour = str(__colour)
+
+    def get_model(self):
+        return f"Модель: {self.__model}"
+
+    def get_hoursepower(self):
+        return f"Мощность двигателя: {self.__engine_power} л. с."
+
+    def get_colour(self):
+        return f"Цвет: {self.__colour}"
+
+    def print_info(self):
+        print(f"{self.get_model()} \n{self.get_hoursepower()} \n{self.get_colour()} \nВладелец: {self.owner}")
+
+    def set_colour(self, new_colour):
+        self.new_colour = str(new_colour)
+        marker = False
+        for i in self.__COLOR_VARIANTS:
+            if new_colour.upper() == i.upper():
+                self.__colour = i
+                marker = True
+        if not marker:
+            print(f"Нельзя сменить цвет на {new_colour}")
+
+
+class Sedan(Vehicle):
+    __PASSENGERS_LIMIT = 5
+
+
+vehicle1 = Sedan('Fedos', 'Toyota Mark II', 500, "blue")
+vehicle1.print_info()
+vehicle1.set_colour('Pink')
+vehicle1.set_colour('BLACK')
+vehicle1.owner = 'Vasyok'
+vehicle1.print_info()
--- nassled.py
+++ nassled.py
@@ -1,25 +1,41 @@
 class Human: # При наследовании, этот класс стал родительским, а тот, что наследует - дочерним.
     head = True
+    _legs = True
+    __arms = True # "__" защищает от переопределения переменной в других классах
 
     def say_hello(self):
         print("Здравствуйте")
 
+    def about(self):
+        print(self.head)
+        print(self._legs)
+        print(self.__arms)
+
 
 class Student(Human): # в скобках указываю откуда будет "наследоваться" наш класс.
     # Дочерний класс. Наследует атрибуты родительского класса
-    def about(self):
-        print("Я студент")
+    pass
+    # def about(self):
+    #     print("Я студент")
+
 
 class Teacher(Human):
     pass
 
 
+# human = _Human()
+# student = Student()
+# teacher = Teacher()
+# print(human.head) # True
+# student.about() # вызов метода класса. # Я студент
+# print(student.head) # True
+# # human.about() # AttributeError: 'Human' object has no attribute 'about'
+# teacher.say_hello()
+# student.say_hello()
 human = Human()
+human.about()
+
 student = Student()
-teacher = Teacher()
-print(human.head) # True
-student.about() # вызов метода класса. # Я студент
-print(student.head) # True
-# human.about() # AttributeError: 'Human' object has no attribute 'about'
-teacher.say_hello()
-student.say_hello()
+student.about()
+
+print(dir(human))
